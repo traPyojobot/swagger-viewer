b5c00fb6ce8f1a3fa50a7699651f21e38b698718
hijiki51 <hibiki0719euph@gmail.com>
2021-04-29 18:51:02 +0900

    change urls

--- src/urls.js
+++ src/urls.js
@@ -1,42 +1,6 @@
 export default [
   {
-    name: 'traQ v3 API',
-    url: 'https://raw.githubusercontent.com/traPtitech/traQ/master/docs/v3-api.yaml'
+    name: "traPyojoBOT Gateway API",
+    url: "https://raw.githubusercontent.com/traPyojobot/gateway/swagger/docs/swagger.yaml",
   },
-  {
-    name: 'traQ v2 API',
-    url: 'https://raw.githubusercontent.com/traPtitech/traQ/master/docs/swagger.yaml'
-  },
-  {
-    name: 'booQ',
-    url: 'https://raw.githubusercontent.com/traPtitech/booQ/master/docs/swagger.yml'
-  },
-  {
-    name: 'anke-to',
-    url: 'https://raw.githubusercontent.com/traPtitech/anke-to/master/docs/swagger/swagger.yaml'
-  },
-  {
-    name: 'knoQ',
-    url: 'https://raw.githubusercontent.com/traPtitech/knoQ/master/docs/swagger.yaml'
-  },
-  {
-    name: 'Jomon',
-    url: 'https://raw.githubusercontent.com/traPtitech/Jomon/master/docs/swagger.yaml'
-  },
-  {
-    name: 'traPortfolio',
-    url: 'https://raw.githubusercontent.com/traPtitech/traPortfolio/master/docs/swagger/traPortfolio.v1.yaml'
-  },
-  {
-    name: 'traP Collection',
-    url: 'https://raw.githubusercontent.com/traPtitech/trap-collection-server/develop/docs/swagger/openapi.yml'
-  },
-  {
-    name: 'NeoShowcase',
-    url: 'https://raw.githubusercontent.com/traPtitech/NeoShowcase/master/api/http/swagger.yaml'
-  },
-  {
-    name: 'Emoine',
-    url: 'https://raw.githubusercontent.com/traPtitech/Emoine/master/docs/openapi.yml'
-  }
-]
+];
